276c3f987417e26a9abc411ac8b8393c32787ae4
sureshPersonal <btech.sharma@gmail.com>
2021-05-06 01:00:26 +0530

    cool

--- src/test/java/myFirstPackage/MyClass.java
+++ src/test/java/myFirstPackage/MyClass.java
@@ -10,6 +10,7 @@ public class MyClass {
 		System.out.println("Auto-Another pipeline check...");
 		System.out.println("This is for autocheck");
 		System.out.println("Hello");
+		System.out.println("4th Line cool....");
 	}
 
 }
